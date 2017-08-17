885e82e79f7fa36813ce9a23d2f08092d20d88ca
yzh <yuzihao@ict.ac.cn>
2017-08-18 00:15:17 +0800

    tests,event: use loop to delay

--- tests/events/events.c
+++ tests/events/events.c
@@ -2,17 +2,23 @@
 
 int main(){
   FILE *fp = fopen("/dev/events", "r");
+  volatile int j = 0;
   while(1){
-    char buf[256];
-    char *p = buf, ch;
-    while((ch = fgetc(fp)) != -1){
-      *p ++ = ch;
-      if(ch == '\n') {
-        *p = '\0';
-        break;
+    j ++;
+    if (j == 1000000) {
+      char buf[256];
+      char *p = buf, ch;
+      while((ch = fgetc(fp)) != -1){
+        *p ++ = ch;
+        if(ch == '\n') {
+          *p = '\0';
+          break;
+        }
       }
+
+      printf("receive event: %s", buf);
+      j = 0;
     }
-    printf("receive event: %s", buf);
   }
 
   fclose(fp);
